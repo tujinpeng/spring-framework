546822517acf7a29ab4d0f9b9e8d823875a40040
tujinpeng <2363695612@qq.com>
2017-07-24 14:01:10 +0800

    创建新spring 学习分之 基于spring3.2.x

--- spring-context/src/main/java/org/springframework/context/support/ClassPathXmlApplicationContext.java
+++ spring-context/src/main/java/org/springframework/context/support/ClassPathXmlApplicationContext.java
@@ -134,7 +134,7 @@ public class ClassPathXmlApplicationContext extends AbstractXmlApplicationContex
 			throws BeansException {
 
 		super(parent);
-		//加载spring的配置文件 例如“applicationContext.xml”
+		// 加载spring的配置文件 例如“applicationContext.xml”
 		setConfigLocations(configLocations);
 		if (refresh) {
 			//初始化bean
